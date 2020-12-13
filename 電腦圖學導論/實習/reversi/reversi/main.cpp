e2a192c4c321dc1d17b82d409a16164cb5fa86aa
郝 廖 <frakwu@gmail.com>
2020-12-13 17:37:26 +0800

    背景音樂(循環播放)

--- 電腦圖學導論/實習/reversi/reversi/main.cpp
+++ 電腦圖學導論/實習/reversi/reversi/main.cpp
@@ -32,11 +32,45 @@
 #endif
 
 
+#define MINIAUDIO_STATIC
+#define MINIAUDIO_IMPLEMENTATION
+#include <miniaudio.h>
 #include "reversi.h"
+
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
+//void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+//{
+//    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
+//    if (pDecoder == NULL) {
+//        return;
+//    }
+//
+//    ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount);
+//
+//    (void)pInput;
+//}
+void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+{
+    ma_bool32 isLooping = MA_TRUE;
+
+    ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
+    if (pDecoder == NULL) {
+        return;
+    }
+
+    /*
+    A decoder is a data source which means you can seemlessly plug it into the ma_data_source API. We can therefore take advantage
+    of the "loop" parameter of ma_data_source_read_pcm_frames() to handle looping for us.
+    */
+    ma_data_source_read_pcm_frames(pDecoder, pOutput, frameCount, NULL, isLooping);
+
+    (void)pInput;
+}
+
+
 void display()
 {
     glColor3b(100,100,100);
@@ -117,6 +151,34 @@ int main(int, char**)
     bool game_window_open = true;
 
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    ma_result result;
+    ma_decoder decoder;
+    ma_device_config deviceConfig;
+    ma_device device;
+    result = ma_decoder_init_file("bounce.wav", NULL, &decoder);
+    if (result != MA_SUCCESS) {
+        return -2;
+    }
+    deviceConfig = ma_device_config_init(ma_device_type_playback);
+    deviceConfig.playback.format = decoder.outputFormat;
+    deviceConfig.playback.channels = decoder.outputChannels;
+    deviceConfig.sampleRate = decoder.outputSampleRate;
+    deviceConfig.dataCallback = data_callback;
+    deviceConfig.pUserData = &decoder;
+    if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
+        printf("Failed to open playback device.\n");
+        ma_decoder_uninit(&decoder);
+        return -3;
+    }
+
+    if (ma_device_start(&device) != MA_SUCCESS) {
+        printf("Failed to start playback device.\n");
+        ma_device_uninit(&device);
+        ma_decoder_uninit(&decoder);
+        return -4;
+    }
+
 
     ImFont* font = io.Fonts->AddFontFromFileTTF("font/NotoSansHans-Regular.ttf", 15.0f, NULL, io.Fonts->GetGlyphRangesChineseFull());
     io.Fonts->GetGlyphRangesChineseSimplifiedCommon();
@@ -251,7 +313,8 @@ int main(int, char**)
         display();
 
     }
-
+    ma_device_uninit(&device);
+    ma_decoder_uninit(&decoder);
     // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
